7a704aaac45ee7aa3fa02e907cfc90d05a816277
rithikav2021 <rithika.v2021@gmail.com>
2026-08-01 12:46:54 +0530

    Completed Spring Boot backend setup

--- backend/src/main/java/com/ecotrack/backend/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.ecotrack.backend.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
